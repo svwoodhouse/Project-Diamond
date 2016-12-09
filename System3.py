56390f3f784affdcb414278d3ff57751ab0b7351
Sydnee Woodhouse <svwoodhouse@gmail.com>
2016-12-09 01:49:20 -0500

    Completed checksum encryption check

--- System3.py
+++ System3.py
@@ -1,5 +1,5 @@
 #This system receives JSON payload from System 2 via SFTP and sends it to System 4 using Pyro4 and encrypts it using Compression with CRC
-
+import hashlib
 import pysftp
 
 #Code that receives the file via SFTP and places it in the local directory
@@ -10,13 +10,18 @@ cinfo = {'cnopts':cnopts, 'host':'oz-ist-linux.abington.psu.edu','username':'ftp
 try:
 	with pysftp.Connection(**cinfo) as sftp:
 		try:
-			sftp.cd('/home/ftpuser')
-			sftp.get('payload.json')
-			sftp.close()
+			with sftp.cd('/home/ftpuser'):
+				sftp.get('payload.json')
 		except:
 			print "File Transfer issue"
 except Exception, err:
 	print err
 	
-
 print "Received Data"
+
+def checkHash(checksum):
+	check = hashlib.md5(open('payload.json','rb').read()).hexdigest()
+	if(check == checksum):	
+		print("Passed checksum test")
+	else:
+		print("Failed checksum test")
